4e6434b56c59dae7b2fa74ffd78ba420a82769a7
DonzKrieg <iromdhon2004@gmail.com>
2025-04-07 19:58:42 +0700

    menambahkan class DataMahasiswa103022330150

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using tpmodul7_kelompok_5;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        DataMahasiswa103022330150 dataMahasiswa = new DataMahasiswa103022330150();
+        dataMahasiswa.ReadJSON();
+    }
+}
